a590f8a4d27ca4c48293121adeb17deadf3e3d82
Praphulkatlana <praphul.katlana@nagarro.com>
2023-09-11 13:50:22 +0530

    Added header

---
+++ src/Common/Header/Header.tsx
@@ -0,0 +1,25 @@
+import './Header.scss'
+const Header = () => {
+  return (
+    <header>
+        <div className="logo">
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQO_rlAHiv8LaaDjVyfY2hrHL2KdrL5-JtKYDqG3wtt&s" alt="logo"/>
+        </div>
+        <div className="navlink">
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Menu</li>
+                <li>Contact</li>
+            </ul>
+
+        </div>
+        <div className="account">
+            <p>signup</p>
+        </div>
+
+    </header>  
+    )
+}
+
+export default Header
---
+++ src/Main.tsx
@@ -0,0 +1,14 @@
+import Header from "./Common/Header/Header"
+import HomeIndex from "./Home/HomeIndex"
+
+const Main = () => {
+  return (
+    <div>
+        <Header/>
+        <HomeIndex/>
+
+    </div>
+  )
+}
+
+export default Main
